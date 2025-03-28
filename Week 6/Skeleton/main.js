7f0703caa4b24f1759e3fccb349f3ae9ebb2334a
Davey <dtdyer123@gmail.com>
2025-03-28 13:08:42 +1100

    Week 6 update Rewrite
    Fixed index to use consistent layout and fixed bug inside updateBunny()

--- Week 6/Skeleton/main.js
+++ Week 6/Skeleton/main.js
@@ -6,9 +6,59 @@ import {mouse, holding, getNextColour} from "/helpers.js";
 setScene();
 setSceneElements();
 setSceneLighting();
+loadMesh();
+let mesh = null;
+
+function loadMesh() {
+    const loader = new PLYLoader();
+    loader.load("models/bunny.ply", function(geometry) {
+        geometry.computeVertexNormals();
+
+        const material = new THREE.MeshPhongMaterial({
+            color: new THREE.Color(1,1,1),
+            shininess: 0
+        });
+        mesh = new THREE.Mesh(geometry, material);
+        mesh.applyMatrix4(new THREE.Matrix4().makeScale(25,25,25));
+        mesh.name = "bunny";
+        scene.add(mesh);
+    })
+}
 
 renderer.setAnimationLoop(UpdateScene);
 
 function UpdateScene() {
+    updateBunny();
     renderer.render(scene, camera);
+}
+
+function doesIntersect(intersects, name) {
+    for (const obj of intersects) {
+        if (obj.object.name === name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function updateBunny() {
+    if (mesh == null) { return; }
+
+    const raycaster = new THREE.Raycaster();
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(scene.children, false);
+
+    if (holding) {
+        if (intersects.length > 0 && doesIntersect(intersects, "bunny")) {
+            mesh.position.x = intersects[0].point.x;
+            mesh.position.y = intersects[0].point.y;
+        }
+    }
+    else {
+        if (!doesIntersect(intersects, "floor") && doesIntersect(intersects, "bunny")) {
+            mesh.position.x = 0;
+            mesh.position.y = 0;
+        }
+    }
+    mesh.material.color = holding ? getNextColour() : new THREE.Color(1,1,1);
 }
