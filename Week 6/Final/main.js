37164ffd0647f0f30ccc1048fb85bc2528b871bd
Davey Dyer <69884791+DaveyD24@users.noreply.github.com>
2025-03-28 13:11:39 +1100

    Update main.js

--- Week 6/Final/main.js
+++ Week 6/Final/main.js
@@ -1,5 +1,5 @@
 import * as THREE from "/build/three.module.js";
-import {scene, camera, renderer, setScene, setSceneElements, setSceneLighting} from "./setup.js";
+import {scene, camera, renderer, setScene, setSceneElements, setSceneLighting} from "/setup.js";
 import {PLYLoader} from "./build/loaders/PLYLoader.js";
 import {mouse, holding, getNextColour} from "/helpers.js";
 
@@ -7,47 +7,58 @@ setScene();
 setSceneElements();
 setSceneLighting();
 loadMesh();
-
-renderer.setAnimationLoop(UpdateScene);
-
 let mesh = null;
+
 function loadMesh() {
     const loader = new PLYLoader();
-    loader.load('models/bunny.ply', function(geometry)
-    {
+    loader.load("models/bunny.ply", function(geometry) {
         geometry.computeVertexNormals();
 
         const material = new THREE.MeshPhongMaterial({
-            color: new THREE.Color(0.9,0.9,0.9),
-            shininess: 0,
+            color: new THREE.Color(1,1,1),
+            shininess: 0
         });
         mesh = new THREE.Mesh(geometry, material);
-        mesh.applyMatrix4(new THREE.Matrix4().makeScale(25, 25, 25));
+        mesh.applyMatrix4(new THREE.Matrix4().makeScale(25,25,25));
         mesh.name = "bunny";
-
         scene.add(mesh);
-    });
+    })
 }
+
+renderer.setAnimationLoop(UpdateScene);
+
 function UpdateScene() {
-    moveBunny();
+    updateBunny();
     renderer.render(scene, camera);
 }
 
-function moveBunny() {
-    if (mesh == null) { return; } //function may run before mesh is loaded
+function doesIntersect(intersects, name) {
+    for (const obj of intersects) {
+        if (obj.object.name === name) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function updateBunny() {
+    if (mesh == null) { return; }
+
     const raycaster = new THREE.Raycaster();
-    raycaster.setFromCamera(mouse, camera); //draw from camera towards mouse position
+    raycaster.setFromCamera(mouse, camera);
     const intersects = raycaster.intersectObjects(scene.children, false);
 
-    if (holding && intersects.length > 0 && intersects[0].object.name === "bunny") {
-        mesh.position.x = intersects[0].point.x;
-        mesh.position.y = intersects[0].point.y;
+    if (holding) {
+        if (intersects.length > 0 && doesIntersect(intersects, "bunny")) {
+            mesh.position.x = intersects[0].point.x;
+            mesh.position.y = intersects[0].point.y;
+        }
     }
-    else if (!holding) {
-        if (intersects.length < 2) { //Means its ONLY intersecting the bunny
+    else {
+        if (!doesIntersect(intersects, "floor") && doesIntersect(intersects, "bunny")) {
             mesh.position.x = 0;
             mesh.position.y = 0;
         }
     }
-    mesh.material.color = holding ? getNextColour() : new THREE.Color(0.9,0.9,0.9);
+    mesh.material.color = holding ? getNextColour() : new THREE.Color(1,1,1);
 }
