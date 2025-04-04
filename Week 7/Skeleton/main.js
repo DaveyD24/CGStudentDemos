ee6a69ec22835f93f79b919cc4bcf2f7a8fd7720
Davey Dyer <69884791+DaveyD24@users.noreply.github.com>
2025-04-04 12:47:59 +1100

    Fixed Skeleton to have correct import

--- Week 7/Skeleton/main.js
+++ Week 7/Skeleton/main.js
@@ -1,7 +1,7 @@
 import * as THREE from "/build/three.module.js";
 import {OrbitControls} from "/build/controls/OrbitControls.js";
 import {scene, camera, renderer, gui, setScene, setSceneLighting} from "./setup.js";
-import {GetGridPosition} from "./Helpers.js";
+import {GetGridPosition, SpawnBox} from "./Helpers.js";
 
 setScene();
 setSceneLighting();
